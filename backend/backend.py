96f2791186d8bc4fd1b1d7a3568e035756593627
ash-047 <147508168+ash-047@users.noreply.github.com>
2024-11-14 14:27:25 +0530

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -1,16 +1,1 @@
-import mysql.connector
-from datetime import datetime
-import ollama
-import json
-from fpdf import FPDF
-
-def workDescriptionsSummarizer(work_descriptions):
-    response = ollama.chat(model = 'llama3.2', messages = [
-    {
-        'role': 'user',
-        'content': f'Please summarize all the work done by a certain Teaching Assistant given the work done by the day everytime he is on the clock: {work_descriptions}. Please ensure that the summary is provided in a json format: "Summary: [point1, point2, ..]"',
-    },
-    ])
-    return response['message']['content']
-
 
